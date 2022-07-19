9e14b4bc86e4cc6e78cee79b3bd505b1bc2f02e9
Вербовский Сергей <verbovskii@inbox.ru>
2022-07-19 23:38:53 +0500

    cook_book complete

--- main.py
+++ main.py
@@ -1,2 +1,40 @@
-with open('recipe.txt') as file:
-    
+from pprint import pprint
+
+cook_book = {}
+
+with open('recipe.txt', encoding='utf-8') as file:
+    for recipe in file:
+        recipes = recipe.strip()
+        ingridients = int(file.readline(2))
+        recipe_list = []
+        for i in range(ingridients):
+            ingredient_name, quantity, measure = file.readline().strip().split(" | ")
+            recipe_list.append({"ingredient_name": ingredient_name, "quantity": quantity, "measure": measure})
+        cook_book[recipes] = recipe_list
+        file.readline()
+
+pprint(cook_book, sort_dicts=False, width=150)
+
+
+def get_shop_list_by_dishes(dishes):
+    dishes_list = []
+    for k, v in cook_book.items():
+        dishes_list.append(k)
+    ingredient_dict = {}
+    ingredient_list = []
+    for i in dishes:
+        if i not in dishes_list:
+            print('There is no such dish on the menu!')
+            break
+    for k, v in cook_book.items():
+        ingredient_list.append(v)
+    for item in ingredient_list:
+        for j in item:
+            for k, v in j.items():
+                print(k, v)
+
+
+    print(ingredient_list)
+
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Фахитос'])
